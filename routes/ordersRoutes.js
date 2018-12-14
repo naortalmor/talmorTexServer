663d27a8d791804c141b514bff186b252491a8f7
Naor Talmor <naortalmornew@gmail.com>
2018-12-14 18:15:05 +0200

    connected to DB

--- routes/ordersRoutes.js
+++ routes/ordersRoutes.js
@@ -31,5 +31,19 @@ ordersRoutes.route('/')
             }
         })
     });
+ordersRoutes.route('/delete').post((req, res, next) => {
+    Orders.deleteOne({id: req.body.order.id}, (err, response) => {
+        if (err) {
+            console.log(err)
+        } else {
+            res.send(response)
+        }
+
+        res.send(req.body.order)
+    })
+})
+    .get((req, res) => {
+        res.send('ok');
+    });
 
 module.exports = ordersRoutes;
