49b078149c55605fed0b63182032eac21109aa49
naor <naortalmornew@gmail.com>
2018-12-27 20:35:36 +0200

    connect to db without mongoose

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
 import * as express from 'express';
+import * as MongoClient from 'mongodb';
 
 class App {
     public express;
+    public static orm;
 
     constructor() {
         this.express = express();
         this.initApp();
+        this.initDBConnection();
+        console.log(App.orm);
     }
 
     initApp(): void {
@@ -15,6 +19,18 @@ class App {
         });
 
         this.express.use('/', router);
+    }
+
+    initDBConnection() {
+        const uri = 'mongodb+srv://mongodb-stitch-talmortex-ovete:naor@cluster0-r43t1.mongodb.net/TalmorTex?retryWrites=true/orders';
+        MongoClient.connect(uri, (err, db) => {
+            if(err) {
+                console.log('There was an error with connection to DB');
+            } else {
+                App.orm = db;
+                console.log('connected to DB')
+            }
+        });
     }
 }
 
